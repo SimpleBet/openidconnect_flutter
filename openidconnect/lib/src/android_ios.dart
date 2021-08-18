06bb28bf05200c1e13257a8a58d90cd248b377b0
Manuel P <manuel@simplebet.io>
2021-08-18 02:54:13 -0400

    user agent edit

--- openidconnect/lib/src/android_ios.dart
+++ openidconnect/lib/src/android_ios.dart
@@ -28,6 +28,8 @@ class OpenIdConnectAndroidiOS {
             height:
                 min(popupHeight.toDouble(), MediaQuery.of(context).size.height),
             child: flutterWebView.WebView(
+              userAgent:
+                  'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
               javascriptMode: flutterWebView.JavascriptMode.unrestricted,
               initialUrl: authorizationUrl,
               onPageFinished: (url) {
